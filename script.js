e8923fa2995f73fb873027f6db11651cb8fd87aa
rabia-aaziz <39001798+rabia-aaziz@users.noreply.github.com>
2018-10-08 17:47:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// write your JS code here
+let alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N'];
+function show(){
+    let alpha = document.querySelector(".show-msg");
+    alpha.innerHTML += `p`;
+}
+   
+
